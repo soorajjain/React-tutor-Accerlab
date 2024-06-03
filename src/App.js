fd88fc887d6f1582a0feb248809641408aee55e6
soorajain <soorajjain51@gmail.com>
2024-06-03 12:09:15 +0530

    final

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1> Sooraj Jain {10+10} Moodubidre</h1> 
+    </div>
+  );
+}
+
+export default App;
