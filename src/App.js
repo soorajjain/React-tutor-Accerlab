6dac6b9781c1cb5c439207076eceb64cbc1408bd
soorajain <soorajjain51@gmail.com>
2024-06-05 10:49:30 +0530

    process

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Parent from "./Parent";
-import Child from "./Child";
+// import Parent from "./Parent";
+// import Child from "./Child";
 // import Test from "./Test";
 // import Child from "./Child";
 // import Parent from "./Parent";
@@ -20,17 +20,43 @@ function App() {
   // let bool = true;
   // let x= 10;
 
+  // let cars = ["suzuki", "homdsa", "hundai"];
+
+  // const onChanges = (e) => {
+  //   console.log(e.target.value);
+  // };
+
+  // const handleChange = (e) => {
+  //   e.preventDefault();
+  // };
+
+  // const handleSubmit = () => {
+  //   localStorage.setItem("name", name);
+  //   localStorage.getItem("name");
+  // };
+
   return (
     <div className="App">
       {/* <Test /> */}
       {/* <Parent /> */}
       {/* <button onClick={ handleSubmit("hello this is sooraj") }>Submit</button> */}
-
       {/* {bool ? <Parent /> : <Child />} */}
-
       {/* <h1>{x>=10 ? "true" : "flase" }</h1> */}
+      {/* <ul>
+        {cars.map((car) => (
 
-      {}
+        
+          <li> {car} </li>
+        ))}
+      </ul> */}
+
+      {/* <form>
+        <label>enter the name:</label>
+        <input type="text" onChange={onChanges} />
+        <button type="text" onClick={handleChange}>
+          Submit
+        </button>
+      </form> */}
     </div>
   );
 }
